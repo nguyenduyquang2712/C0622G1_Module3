f7d58a3a232306333a4c043b62de53ddede7bf84
nguyenduyquang2712 <quangkg8@gmail.com>
2022-09-13 23:51:06 +0700

    update

--- sql/case_study/case_study.sql
+++ sql/case_study/case_study.sql
@@ -171,5 +171,32 @@ VALUES (1, '2020-12-08', '2020-12-08', 0, 3, 1, 3),
 (12, '2021-05-25', '2021-05-27', 0, 7, 10, 1);
 INSERT INTO hop_dong_chi_tiet(ma_hop_dong_chi_tiet,ma_hop_dong,ma_dich_vu_di_kem,so_luong)
 VALUE(1,2,4,5),(2,2,5,8),(3,2,6,15),(4,3,1,1),(5,3,2,11),(6,1,3,1),(7,1,2,2),(8,12,2,2);
-SELECT * FROM nhan_vien WHERE (ho_ten like'T%' OR ho_ten LIKE 'H%' OR ho_ten LIKE 'K%') AND length(ho_ten)<15;
-SELECT * FROM khach_hang WHERE (TIMESTAMPDIFF(YEAR, ngay_sinh, CURDATE()) BETWEEN 18 AND 50) AND (dia_chi LIKE '%Đà Nẵng%' or dia_chi LIKE '%Quảng Trị%');
+
+-- task 2
+SELECT * FROM nhan_vien WHERE (ho_ten  regexp '^[HKT]') AND char_length(ho_ten)<=15; 
+
+-- task3
+SELECT * FROM khach_hang WHERE (TIMESTAMPDIFF(YEAR, ngay_sinh, CURDATE()) BETWEEN 18 AND 50) AND (dia_chi LIKE '%Đà Nẵng%' OR dia_chi LIKE '%Quảng Trị%');
+
+-- task4
+SELECT  khach_hang.ma_khach_hang, khach_hang.ho_ten,COUNT(hop_dong.ma_khach_hang) as so_lan_dat_phong FROM khach_hang 
+INNER JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+INNER JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
+WHERE (loai_khach.ten_loai_khach="Diamond") GROUP BY hop_dong.ma_khach_hang ORDER BY so_lan_dat_phong ;
+
+-- task5
+SELECT khach_hang.ma_khach_hang,khach_hang.ho_ten,loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong,hop_dong.ngay_ket_thuc, (ifnull(dich_vu.chi_phi_thue,0)+ ifnull(hop_dong_chi_tiet.so_luong,0)*ifnull(dich_vu_di_kem.gia,0)) as tong_tien
+FROM khach_hang
+LEFT JOIN loai_khach ON khach_hang.ma_loai_khach = loai_khach.ma_loai_khach 
+LEFT JOIN hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+LEFT JOIN dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+LEFT JOIN hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+LEFT JOIN dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+GROUP BY hop_dong.ma_hop_dong;
+
+-- task6
+
+
+
+
+
--- sql/ss1_csdl_quan_he/exercises/student_table.sql
+++ sql/ss1_csdl_quan_he/exercises/student_table.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE IF NOT EXISTS student_management;
 USE student_management;
 DROP TABLE IF EXISTS student;
-CREATE TABLE `student` (
-  `id` int NOT NULL,
+CREATE TABLE student (
+  id int NOT NULL,
   `name` varchar(45) DEFAULT NULL,
-  `age` int DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  age int DEFAULT NULL,
+  country varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 DROP TABLE IF EXISTS class;
 CREATE TABLE class (
-  `id` int NOT NULL,
+  id int NOT NULL,
   `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (id)
 );
 DROP TABLE IF EXISTS teacher;
 CREATE TABLE teacher (
-  `id` int NOT NULL,
+  id int NOT NULL,
   `name` varchar(45) DEFAULT NULL,
-  `age` int DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  age int DEFAULT NULL,
+  country varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
 )
