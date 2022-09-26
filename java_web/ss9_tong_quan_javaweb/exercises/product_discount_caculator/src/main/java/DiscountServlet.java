2be81871b8a17cdaa876a52fcc6b014f4cfb8a26
nguyenduyquang2712 <quangkg8@gmail.com>
2022-09-26 16:17:25 +0700

    update

---
+++ java_web/ss9_tong_quan_javaweb/exercises/product_discount_caculator/src/main/java/DiscountServlet.java
@@ -0,0 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+
+@WebServlet(name = "DiscountServlet", urlPatterns = "/discount")
+public class DiscountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        String price = request.getParameter("price");
+        String discount = request.getParameter("discount");
+        String description = request.getParameter("description");
+        BigDecimal discountAmount = new BigDecimal(Double.parseDouble(price) * Double.parseDouble(discount) * 0.01);
+        BigDecimal discountPrice = new BigDecimal(Double.parseDouble(price) - Double.parseDouble(price) * Double.parseDouble(discount)*0.01);
+        request.setAttribute("description",description);
+        request.setAttribute("price",price);
+        request.setAttribute("discount",discount);
+        request.setAttribute("discountAmount",discountAmount);
+        request.setAttribute("discountPrice",discountPrice);
+        request.getRequestDispatcher("/result.jsp").forward(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ java_web/ss9_tong_quan_javaweb/exercises/product_discount_caculator/src/main/webapp/result.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: QUANG
+  Date: 9/26/2022
+  Time: 2:28 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Product Discount</title>
+</head>
+<body>
+<h1 >Description : ${description}</h1>
+<h1>Price = ${price}</h1>
+<h1>Discount = ${discount}</h1>
+<h1>Discount Amount = ${discountAmount}</h1>
+<h1>Discount Price = ${discountPrice}</h1>
+</body>
+</html>
